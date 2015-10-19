ff01a16e47552b9849cb5481a013c8bfaa75c373
Robin <nixdmn@gmail.com>
2015-10-19 18:57:44 -0500

    added project files

---
+++ Core/Enumerations.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Core
+{
+	public enum CardType
+	{
+		Debit = 1,
+		Credit = 2,
+		Gift = 3,
+		None = -1
+	}
+
+	public enum Months
+	{
+		Jan = 1,
+		Feb = 2,
+		Mar = 3,
+		Apr = 4,
+		May = 5,
+		Jun = 6,
+		Jul = 7,
+		Aug = 8,
+		Sep = 9,
+		Oct = 10,
+		Nov = 11,
+		Dec = 12
+	}
+
+} // namespace
