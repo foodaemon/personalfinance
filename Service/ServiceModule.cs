15f157aeebb556e58935df630789f06b9d3b8ea8
Robin <nixdmn@gmail.com>
2015-10-24 21:00:55 -0500

    updated project with ninject

---
+++ Service/ServiceModule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Ninject.Modules;
+using Data;
+using Core.Domains;
+
+namespace Service
+{
+	public class ServiceModule: NinjectModule
+	{
+		public override void Load()
+		{
+			Bind<IRepository<Account>>().To<Repository<Account>>();
+			Bind<IRepository<Category>>().To<Repository<Category>>();
+		}
+	} // class
+} // namespace
---
+++ Web/Infrastructure/WebModule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Ninject.Modules;
+using Service;
+using Service.Interfaces;
+
+namespace Web.Infrastructure
+{
+	public class WebModule : NinjectModule
+	{
+		public override void Load()
+		{
+			Bind<IAccountService>().To<AccountService>();
+			Bind<ICategoryService>().To<CategoryService>();
+		}
+	} // class
+} // namespace
