8ae06ec031ed14c65db38d23abcc661d48c58711
Robin <nixdmn@gmail.com>
2015-11-05 20:44:30 -0600

    added logger class and interface

---
+++ Core/Logging/ILogger.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Core
+{
+	public interface ILogger
+	{
+		void Info(string message);
+
+		void Debug(string message);
+
+		void Error(string message);
+
+		void Trace(string message);
+	}
+}
---
+++ Core/Logging/Logger.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Core
+{
+	public class Logger : ILogger
+	{
+		public void Info (string message)
+		{
+			throw new NotImplementedException ();
+		}
+		public void Debug (string message)
+		{
+			throw new NotImplementedException ();
+		}
+		public void Error (string message)
+		{
+			throw new NotImplementedException ();
+		}
+		public void Trace (string message)
+		{
+			throw new NotImplementedException ();
+		}
+	} // class
+} // namespace
